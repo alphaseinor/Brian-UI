6fa9e41c2bf9ffbd6c6c5c10f8402d9f7b3a9468
alphaseinor <alphaseinor@gmail.com>
2019-08-22 16:46:03 -0500

    added onclick event to close modal

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.nav-item');
 
+//makes close button on modal clickable
+const modalMenuButton = document.querySelector('.modal-menu-btn');
+const loginBox = document.querySelector('.login-box');
+
+//menu section does stuff
 //initial state of menu is hidden
 let showMenu = false;
 
@@ -16,11 +21,30 @@ function toggleMenu() {
     menuNav.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
     showMenu = true;
+    console.log('close menu active');
   } else {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
     showMenu = false;
+    console.log('open menu active');
+  }
+}
+
+//login click event handler
+modalMenuButton.addEventListener('click', closeModal);
+
+let showLoginBox = false;
+
+function closeModal() {
+  if (!showLoginBox === false) {
+    loginBox.classList.add('show');
+    showLoginBox = true;
+    console.log('close button is visible');
+  } else {
+    loginBox.classList.remove('show');
+    showLoginBox = false;
+    console.log('close button is not visible');
   }
 }
